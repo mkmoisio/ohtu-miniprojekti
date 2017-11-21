acb6175878d718317accccc53ff5091dc2f2297a
Mikko Moisio <mikko.moisio@helsinki.fi>
2017-11-21 15:27:19 +0200

    Käyttöliittymätestejä

---
+++ src/main/java/io/Tulostaja.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io;
+
+import java.io.PrintStream;
+
+/**
+ *
+ * @author mikkomo
+ */
+public class Tulostaja implements TulostusRajapinta {
+    private final PrintStream ps;
+
+    public Tulostaja(PrintStream ps) {  
+        this.ps = ps;
+    }
+
+    @Override
+    public void println(String x) {
+        ps.println(x);
+    }
+
+}
--- src/test/java/miniprojekti/kayttoliittyma/KayttoliittymaTest.java
+++ src/test/java/miniprojekti/kayttoliittyma/KayttoliittymaTest.java
@@ -5,6 +5,8 @@
  */
 package miniprojekti.kayttoliittyma;
 
+import Kayttoliittyma.Kayttoliittyma;
+import Vinkkitietokanta.Vinkkitietokanta;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,24 +19,33 @@ import static org.junit.Assert.*;
  * @author mikkomo
  */
 public class KayttoliittymaTest {
-    
+
+    Vinkkitietokanta tietokanta;
+    Kayttoliittyma UI;
+
     public KayttoliittymaTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+        this.tietokanta = new Vinkkitietokanta();
+        this.UI = new Kayttoliittyma(tietokanta);
     }
-    
+
     @After
     public void tearDown() {
     }
 
+    @Test
+    public void kirjavinkinLisaaminenOnnistuuValidillaSyotteellaa() {
+        assertTrue(UI.lisaaKirjavinkki("Cormen", "Introduction.to.Algorithms.3rd.Edition.Sep.2010"));       
+    }
 }
