c1396600ef49ace3c6fe1817e3b2d168bd12dc7b
thvaisa <thvaisa@>
2017-12-13 17:03:05 +0200

    lisaa cucumber-testeja

--- src/test/java/miniprojekti/kayttoliittyma/Stepdefs.java
+++ src/test/java/miniprojekti/kayttoliittyma/Stepdefs.java
@@ -7,6 +7,8 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -162,7 +164,8 @@ public class Stepdefs {
     
     @When("^title \"([^\"]*)\" is entered$")
     public void title_is_entered(String arg1) throws Throwable {
-        l.lisaaSyote(arg1);
+        l.lisaaSyote(arg1)
+                ;
         l.lisaaSyote("lopeta");
         ui.suorita();
     }
@@ -174,16 +177,73 @@ public class Stepdefs {
         ui.suorita();
     }    
     
+    @When("^the user checks the all the books$")
+    public void the_user_checks_the_all_the_books() throws Throwable {
+        l.lisaaSyote("tulosta kirjat");
+        l.lisaaSyote("lopeta");
+        ui.suorita();
+    }
+
+    @Then("^the output contains \"([^\"]*)\"$")
+    public void the_output_contains(String arg1) throws Throwable {
+        
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg1));
+    }
+
+    private boolean listaSisaltaaMerkkijonon(List<String> list, String str){
+        boolean contains = false;
+        for(String str0 : list){
+            if(str0.contains(str)) contains= true; 
+        }
+        return contains;
+    }
     
     
-//      @Then("^the application responds with a list containing tip with url \"([^\"]*)\" and title \"([^\"]*)\"$")
-//    public void response_contains(String url, String title) {
-//        assertTrue(t.tulosteSisaltaa(Muotoilut.));
-//    }
-//    
+
+    @Then("^the application responds with a list containing an unread book tip with author \"([^\"]*)\" and title \"([^\"]*)\"$")
+    public void the_application_responds_with_a_list_containing_an_unread_book_tip_with_author_and_title(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg2));
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg1));
+    }
     
-     /*
-    LISTAUS LOPPUU
+    @Then("^the application responds with a list containing an unread tip with url \"([^\"]*)\" and title \"([^\"]*)\"$")
+    public void the_application_responds_with_a_list_containing_an_unread_tip_with_url_and_title(String arg1, String arg2) throws Throwable {
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg2));
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg1));
+    }
+    
+    
+    @Then("^the application responds with a list containing a read book tip with author \"([^\"]*)\" and title \"([^\"]*)\"$")
+    public void the_application_responds_with_a_list_containing_a_read_book_tip_with_author_and_title(String arg1, String arg2) throws Throwable {
+    assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg2));
+        assertTrue(listaSisaltaaMerkkijonon(t.tulosteetListana(),arg1));
+    
+    }
+
+  
+    
+    /*
+    @When("^url \"([^\"]*)\" and author \"([^\"]*)\" and title \"([^\"]*)\" are entered$")
+    public void url_and_author_and_title_are_entered(String arg1, String arg2, String arg3) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^command \"([^\"]*)\" is entered and title \"([^\"]*)\"Mattihan se sopan keitti\"([^\"]*)\" is entered$")
+    public void command_is_entered_and_title_Mattihan_se_sopan_keitti_is_entered(String arg1, String arg2, String arg3) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^command \"([^\"]*)\" is entered$")
+    public void command_is_entered(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
     */
+
+
 
 }
